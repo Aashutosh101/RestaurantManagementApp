7d7535aa781d0c280fb82c5c4f4828812aab2d2b
Aashutosh Swarnakar <sendaashutosh@gmail.com>
2018-08-07 15:39:58 +0530

    added some controllers

---
+++ src/main/java/com/visa/prj/web/LoginController.java
@@ -0,0 +1,57 @@
+package com.visa.prj.web;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.visa.prj.service.UserService;
+
+@Controller
+public class LoginController {
+
+	@Autowired
+	private UserService userService;
+
+	@RequestMapping("userLogin.do")
+	public void processUserLogin(HttpServletRequest request, HttpServletResponse response) {
+		String email = request.getParameter("email");
+		String password = request.getParameter("pwd");
+		boolean userExists = userService.checkIfUserExist(email, password);
+		try {
+			if (userExists == true) {
+				response.sendRedirect("userHome.html");
+			} else {
+				response.sendRedirect("index.html");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@RequestMapping("adminLogin.do")
+	public void processAdminLogin(HttpServletRequest request, HttpServletResponse response) {
+		String email = request.getParameter("email");
+		String password = request.getParameter("pwd");
+		boolean adminExists = userService.checkIfAdminExist(email, password);
+		try {
+			if (adminExists == true) {
+				response.sendRedirect("adminHome.html");
+			} else {
+				response.sendRedirect("index.html");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@RequestMapping("logout.do")
+	public String processLogout(Model model) {
+		return "index.html";
+	}
+}
